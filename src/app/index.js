d8bcd894d4f351ce1cf5dc120c04073f21a87541
vazquez <2ricardo.vazquez@gmail.com>
2015-02-10 21:39:02 -0500

    Adding about view. No controller

--- src/app/index.js
+++ src/app/index.js
@@ -2,11 +2,17 @@
 
 angular.module('vazquezio', ['ngAnimate', 'ngCookies', 'ui.router', 'ui.bootstrap'])
   .config(function ($stateProvider, $urlRouterProvider) {
+
+    // $locationProvider.html5Mode(true);
     $stateProvider
       .state('home', {
         url: '/',
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'app/about/about.html',
       });
 
     $urlRouterProvider.otherwise('/');
