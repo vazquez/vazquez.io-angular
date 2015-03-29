e8b2afe524b363733c8faad76bd8db8b0e5a8c00
Ricardo Vazquez <ricardo@mozillafoundation.org>
2015-03-29 13:26:30 -0400

    Adding project list array in controller. Modifying view to reflect data in controller.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,5 +2,9 @@
 
 angular.module('vazquezio')
   .controller('MainCtrl', function ($scope) {
-
+    $scope.projects = [
+        {"id": 0, "name": "Mozilla Webmaker", "desc": "A landing page that focuses on improving conversion and enhacing the voice of Mozilla.", "img": "webmaker.jpg"},
+        {"id": 1, "name": "Mozilla Webmaker App", "desc": "A mobile app with a goal of allowing people to easily create digital and publishable content", "img": "webmaker-app.jpg"},
+        {"id": 2, "name": "500px Uploader", "desc": "Uploading photos should be easy. This one does the trick.", "img": "500.jpg"}
+    ];
   });
