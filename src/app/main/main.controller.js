d9a20aacd2a015851c519d5855cf0bc579c7e7c9
Ricardo Vazquez <ricardo@mozillafoundation.org>
2015-04-25 20:14:31 -0400

    Improving Projects section

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -5,6 +5,7 @@ angular.module('vazquezio')
     $scope.projects = [
         {"id": 0, "name": "Mozilla Webmaker", "desc": "A landing page that focuses on improving conversion and enhacing the voice of Mozilla.", "img": "webmaker.jpg"},
         {"id": 1, "name": "Mozilla Webmaker App", "desc": "A mobile app with a goal of allowing people to easily create digital and publishable content", "img": "webmaker-app.jpg"},
-        {"id": 2, "name": "500px Uploader", "desc": "Uploading photos should be easy. This one does the trick.", "img": "500.jpg"}
+        {"id": 2, "name": "FreshBooks iOS 7 App", "desc": "Helping small businesses around the world invoice and track time right from their iPhone.", "img": "500.jpg"}
+        // {"id": 3, "name": "500px Uploader", "desc": "This photo uploader helps place the most beautiful photography in the world up on the web.", "img": "500.jpg"}
     ];
   });
